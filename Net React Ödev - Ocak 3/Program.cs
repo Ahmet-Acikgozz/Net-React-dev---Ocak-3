79c3aedb619d42c42d6641eaba5fa23442d19d61
Ahmet-Acikgozz <aackgz08@gmail.com>
2025-03-07 13:37:44 +0300

    Proje dosyası ekle.

---
+++ Net React Ödev - Ocak 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Lütfen bir sayı giriniz :");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+if (sayi1 < 0)
+{
+    Console.WriteLine("Girdiğiniz sayı NEGATİFTİR ");
+
+}
+else if (sayi1 == 0){
+    Console.WriteLine("Girdiğiniz sayı SIFIRDIR ");
+}
+else if (sayi1 > 0)
+{
+    Console.WriteLine("Girdiğiniz sayı POZİTİFTİR ");
+}
