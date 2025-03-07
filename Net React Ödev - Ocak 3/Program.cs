f97f3b09123dc683b0d8d14b837125ccacafb576
Ahmet-Acikgozz <aackgz08@gmail.com>
2025-03-07 13:44:30 +0300

    tek mi çift mi eklendi

--- Net React Ödev - Ocak 3/Program.cs
+++ Net React Ödev - Ocak 3/Program.cs
@@ -1,4 +1,24 @@
-﻿Console.Write("Lütfen bir sayı giriniz :");
+﻿
+// --------------  Kullanıcıdan bir sayı alıp tek mi çift mi olduğunu bulan bir C# programı yazın
+
+Console.Write("Lütfen bir sayi giriniz : ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girmiş olduğunuz sayı ÇİFT bir sayıdır");
+}
+else
+    Console.WriteLine("Girmiş olduğunuz sayı TEK bir sayıdır");
+
+
+
+
+
+
+// --------------------------- Girilen bir sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın. 
+
+/*Console.Write("Lütfen bir sayı giriniz :");
 int sayi1 = Convert.ToInt32(Console.ReadLine());
 if (sayi1 < 0)
 {
@@ -11,4 +31,4 @@ else if (sayi1 == 0){
 else if (sayi1 > 0)
 {
     Console.WriteLine("Girdiğiniz sayı POZİTİFTİR ");
-}
+} */
